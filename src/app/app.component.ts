fa424407f6f919f8ba80b3d8fa1622c8e032d906
Franco Ojeda <fojedavideos@gmail.com>
2023-01-26 09:56:38 -0500

    calcular con eval para simplificar codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numPantalla1 = 0;
-  numPantalla2 = 0;
+  
+  numPantalla1!:number;
+  numPantalla2!:number;
   
   operacion:any;
-  resultado:any;
+
+  operacion2:any;
+
+  resultado!:number;
+
+  msg = false;
 
   reseteado(){
-    this.resultado = '';
+    this.resultado = 0;
     this.numPantalla1 = 0;
     this.numPantalla2 = 0;
   }
+
+  // Esta fue la opcion 1 para calcular
   calcular(){
-    console.log(this.numPantalla1);
-    console.log(this.operacion);
-    this.resultado = this.numPantalla1 + this.operacion + this.numPantalla2;
+    if (this.operacion == 'sumar') {
+      this.resultado = this.numPantalla1 + this.numPantalla2;
+      console.log(this.resultado);
+      this.msg = false;
+    } else if (this.operacion == 'restar') {
+      this.resultado = this.numPantalla1 - this.numPantalla2;
+      console.log(this.resultado);
+      this.msg = false;
+    } else if (this.operacion == 'multiplicar') {
+      this.resultado = this.numPantalla1 * this.numPantalla2;
+      console.log(this.resultado);
+      this.msg = false;
+    } else if (this.operacion == 'dividir') {
+      this.resultado = this.numPantalla1 / this.numPantalla2;
+      console.log(this.resultado);
+      this.msg = false;
+    } else {
+      this.resultado = 0;
+      this.msg = true;
+    }
+  }
+
+  //Opcion con menos codigo
+  calcular2(){
+    this.resultado = eval(this.numPantalla1 + this.operacion2 + this.numPantalla2);
+    if (!this.resultado) {
+      this.msg = true;
+    } else {
+      this.msg = false;
+    }
   }
 }

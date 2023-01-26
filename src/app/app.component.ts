c638128437dcd6ca4aa9289b6b9810d33afc77ca
Franco Ojeda <fojedavideos@gmail.com>
2023-01-26 09:38:22 -0500

    implementacion de operaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio-27';
+  numPantalla1 = 0;
+  numPantalla2 = 0;
+  
+  operacion:any;
+  resultado:any;
+
+  reseteado(){
+    this.resultado = '';
+    this.numPantalla1 = 0;
+    this.numPantalla2 = 0;
+  }
+  calcular(){
+    console.log(this.numPantalla1);
+    console.log(this.operacion);
+    this.resultado = this.numPantalla1 + this.operacion + this.numPantalla2;
+  }
 }
